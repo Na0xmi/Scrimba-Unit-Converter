c20fac52c4ffacf9c0de7ee2714eddb3e18d0333
Naomiii <66597376+Na0xmi@users.noreply.github.com>
2023-02-18 19:03:45 +0100

    Completed functions
    Added functions to convert volume and mass, adjusted index.html to show default values

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let inputEl = document.getElementById("input-el")
 let lengthEl = document.getElementById("length-el")
+let volumeEl = document.getElementById("volume-el")
+let massEl = document.getElementById("mass-el")
+
 const convertBtn = document.getElementById("convert-btn")
 
 const meterToFeet = 3.281
@@ -9,4 +12,6 @@ const kiloToPound = 2.204
 convertBtn.addEventListener("click", function(){
     let base = inputEl.value
     lengthEl.textContent = `${base} meter = ${(base* meterToFeet).toFixed(3)} feet | ${base} feet = ${(base/meterToFeet).toFixed(3)} meter`
+    volumeEl.textContent = `${base} liter = ${(base * literToGallon).toFixed(3)} gallon | ${base} gallon = ${(base/literToGallon).toFixed(3)} liter`
+    massEl.textContent = `${base} kilo = ${(base*kiloToPound).toFixed(3)} pounds | ${base} pounds = ${(base/kiloToPound).toFixed(3)} kilo`
 })
